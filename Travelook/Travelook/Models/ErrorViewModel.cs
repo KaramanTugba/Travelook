224935cadb5b5d04aa9665f94801349ab82c39a3
Tuğba KARAMAN <97016474+KaramanTugba@users.noreply.github.com>
2022-05-07 01:59:41 +0300

    added: Concrete in EntityLayer
    Main classes added.

---
+++ Travelook/Travelook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Travelook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
